6c414a80875d475f76ccfe7fa299acdb0db13072
LuanaTamborrino2402 <luanatamborrino2402@gmail.com>
2024-03-05 21:57:52 +0100

    Completed register function

--- src/main/java/com/luanatamborrino/SwiftHealthPocket/dto/request/RegisterRequest.java
+++ src/main/java/com/luanatamborrino/SwiftHealthPocket/dto/request/RegisterRequest.java
@@ -15,4 +15,5 @@ public class RegisterRequest {
     private String cognome;
     private String email;
     private String password;
+    private String ruolo;
 }
--- src/main/java/com/luanatamborrino/SwiftHealthPocket/service/AuthenticationService.java
+++ src/main/java/com/luanatamborrino/SwiftHealthPocket/service/AuthenticationService.java
@@ -1,7 +1,7 @@
 package com.luanatamborrino.SwiftHealthPocket.service;
 
 import com.luanatamborrino.SwiftHealthPocket.dto.response.LoginResponse;
-import com.luanatamborrino.SwiftHealthPocket.exception.BadRequestException;
+import com.luanatamborrino.SwiftHealthPocket.exception.*;
 import com.luanatamborrino.SwiftHealthPocket.security.JwtService;
 import com.luanatamborrino.SwiftHealthPocket.dto.request.AuthenticationRequest;
 import com.luanatamborrino.SwiftHealthPocket.dto.request.RegisterRequest;
@@ -15,6 +15,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
@@ -24,18 +26,53 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
+    /**
+     * Metodo per registrare un utente nel database.
+     * @param request DTO con i dati della registrazione
+     */
     public void register(RegisterRequest request) {
-    //TODO fare i soliti controlli di sicurezza
 
+        //Controllo se il ruolo è valido e poi lo assegno.
+        Ruolo ruolo;
+        if(request.getRuolo().equals("PAZIENTE")){
+             ruolo = Ruolo.PAZIENTE;
+        }else if (request.getRuolo().equals("INFERMIERE")) {
+            ruolo = Ruolo.INFERMIERE;
+        }else{
+            throw new BadRequestException("Ruolo non valido.");
+        }
+
+        //Controllo se è già registrato un utente con quella email.
+        Optional<Utente> utenteGiaRegistrato = repository.findByEmail(request.getEmail());
+        if(utenteGiaRegistrato.isPresent()){
+            throw new ConflictException("Email già registrata.");
+        }
+
+        //Controllo se almeno un campo è vuoto.
+        if(request.getNome().isEmpty() || request.getNome().isBlank() ||
+                request.getPassword().isEmpty() || request.getPassword().isBlank() ||
+                request.getEmail().isEmpty() || request.getEmail().isBlank() ||
+                request.getCognome().isEmpty() || request.getCognome().isBlank()){
+            throw new BadRequestException("Campo non inserito.");
+        }
+
+        //Creo un utente.
         Utente utente = Utente.builder()
                 .nome(request.getNome())
                 .cognome(request.getCognome())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .ruolo(Ruolo.PAZIENTE) //TODO il ruolo deve far parte della request (quindi deve sceglierlo l'utente)
+                .ruolo(ruolo)
                 .build();
 
+        //Salvo l'utente creato nel database.
         repository.save(utente);
+
+        //Controllo se l'utente appena registrato è presente effettivamente nel database.
+        Optional<Utente> utenteRegistrato = repository.findByEmail(request.getEmail());
+        if(utenteRegistrato.isEmpty()){
+            throw new InternalServerErrorException("Registrazione fallita.");
+        }
     }
 
     public LoginResponse authenticate(AuthenticationRequest request) {
