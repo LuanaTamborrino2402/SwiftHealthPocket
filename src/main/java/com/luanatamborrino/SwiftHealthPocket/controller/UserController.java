5f3a39490a778710a54cd909250c29c90ca38410
LuanaTamborrino2402 <luanatamborrino2402@gmail.com>
2024-03-19 21:03:08 +0100

    Completed method getAllUsers

--- src/main/java/com/luanatamborrino/SwiftHealthPocket/controller/UserController.java
+++ src/main/java/com/luanatamborrino/SwiftHealthPocket/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.luanatamborrino.SwiftHealthPocket.controller;
 
+import com.luanatamborrino.SwiftHealthPocket.dto.response.MessageResponse;
 import com.luanatamborrino.SwiftHealthPocket.dto.response.UserResponse;
 import com.luanatamborrino.SwiftHealthPocket.service.UtenteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +27,28 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
+    }
+
+    @DeleteMapping("/deleteUserById/{id}")
+    public ResponseEntity<MessageResponse> deleteUserById(@PathVariable String id){
+
+        long userId = Long.parseLong(id);
+
+        userService.deleteUserById(userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new MessageResponse("Utente eliminato."));
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+
+        final List<UserResponse> response = userService.getAllUsers();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+
     }
 }
--- src/main/java/com/luanatamborrino/SwiftHealthPocket/service/UtenteService.java
+++ src/main/java/com/luanatamborrino/SwiftHealthPocket/service/UtenteService.java
@@ -2,12 +2,15 @@ package com.luanatamborrino.SwiftHealthPocket.service;
 
 import com.luanatamborrino.SwiftHealthPocket.dto.response.UserResponse;
 import com.luanatamborrino.SwiftHealthPocket.exception.BadRequestException;
+import com.luanatamborrino.SwiftHealthPocket.exception.InternalServerErrorException;
 import com.luanatamborrino.SwiftHealthPocket.exception.NotFoundException;
 import com.luanatamborrino.SwiftHealthPocket.model.Utente;
 import com.luanatamborrino.SwiftHealthPocket.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -38,5 +41,51 @@ public class UtenteService {
                 user.get().getEmail(),
                 user.get().getRuolo());
     }
+
+    public void deleteUserById(Long id){
+
+        //controllo che l'id parta da 1.
+        if(id < 1) {
+            throw new BadRequestException("Id non corretto.");
+        }
+
+        Optional<Utente> user = repository.findById(id);
+
+        //Se non esiste lancio l'eccezione.
+        if(user.isEmpty()){
+            throw new NotFoundException("Utente non trovato.");
+        }
+
+        repository.deleteById(user.get().getIdUtente());
+
+        Optional<Utente> userDeleted = repository.findById(user.get().getIdUtente());
+        if(userDeleted.isPresent()){
+            throw new InternalServerErrorException("Errore nell'eliminazione.");
+        }
+    }
+
+    //TODO fare il meteodo deleteByEmail
+
+    public List<UserResponse> getAllUsers(){
+
+        List<Utente> userList = repository.findAll();
+        if(userList.isEmpty()){
+            throw new NotFoundException("Utenti non trovati.");
+        }
+
+        List<UserResponse> response = new ArrayList<>();
+
+        //per ogni utente in userList
+        for (Utente user : userList ){
+            response.add(new UserResponse(user.getIdUtente(),
+                    user.getNome(),
+                    user.getCognome(),
+                    user.getPassword(),
+                    user.getEmail(),
+                    user.getRuolo()));
+        }
+        return response;
+    }
+
 
 }
