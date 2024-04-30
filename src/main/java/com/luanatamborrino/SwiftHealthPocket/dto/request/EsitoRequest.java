4075a97fb289a79719cedc07dcfba7f8753f7e5d
LuanaTamborrino2402 <luanatamborrino2402@gmail.com>
2024-04-30 12:30:23 +0200

    DTO

---
+++ src/main/java/com/luanatamborrino/SwiftHealthPocket/dto/request/EsitoRequest.java
@@ -0,0 +1,11 @@
+package com.luanatamborrino.SwiftHealthPocket.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EsitoRequest {
+   private String esito;
+}
+
